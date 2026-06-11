51a884255750b6285f6cd4fc1ca42f09c463b605
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-06-11 13:01:18 -0500

    Caesar Cipher - Finished (?)

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Welcome to the Caesar Cipher Encoder!\n")
+
+num_to_shift = int(input("Enter the number of positions to shift (1-25): "))
+
+while num_to_shift < 1 or num_to_shift > 25:
+    num_to_shift = int(input("Enter the number of positions to shift: "))
+
+word = input("Enter the word to encode: ")
+word = word.upper()
+
+encoded_word = ""
+decoded_word = ""
+
+for letter in word:
+    letter_num = (ord(letter)-65) % 26 + 65
+    letter_num += num_to_shift
+    encoded_letter = chr(letter_num)
+    encoded_word += encoded_letter
+
+print("\nEncoded word: " + encoded_word)
+
+for letter in encoded_word:
+    encoded_letter_num = (ord(letter)-65) % 26 + 65
+    encoded_letter_num -= num_to_shift
+    decoded_letter = chr(encoded_letter_num)
+    decoded_word += decoded_letter
+
+print("\nDecoded word: " + decoded_word)
+
+print("\nThank you for using the Caesar Cipher Encoder!")
