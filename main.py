3f659c26d5878cd6976d9a3d514b2bd5fa52447a
kk2ktr2011-cloud <kk2ktr2011@gmail.com>
2026-06-18 10:59:07 -0500

    Added Upper or Lower Case Checking

--- main.py
+++ main.py
@@ -6,25 +6,35 @@ while num_to_shift < 1 or num_to_shift > 25:
     num_to_shift = int(input("Enter the number of positions to shift: "))
 
 word = input("Enter the word to encode: ")
-word = word.upper()
 
 encoded_word = ""
 decoded_word = ""
 
 for letter in word:
-    letter_num = (ord(letter)-65) % 26 + 65
-    letter_num += num_to_shift
-    encoded_letter = chr(letter_num)
-    encoded_word += encoded_letter
+    if letter.isupper():
+        letter_num = (ord(letter)-65) % 26 + 65
+        letter_num += num_to_shift
+        encoded_letter = chr(letter_num)
+        encoded_word += encoded_letter
+    else:
+        letter_num = (ord(letter)-97) % 26 + 97
+        letter_num += num_to_shift
+        encoded_letter = chr(letter_num)
+        encoded_word += encoded_letter
 
 print("\nEncoded word: " + encoded_word)
 
 for letter in encoded_word:
-    encoded_letter_num = (ord(letter)-65) % 26 + 65
-    encoded_letter_num -= num_to_shift
-    decoded_letter = chr(encoded_letter_num)
-    decoded_word += decoded_letter
-
+    if letter.isupper():
+        encoded_letter_num = (ord(letter)-65) % 26 + 65
+        encoded_letter_num -= num_to_shift
+        decoded_letter = chr(encoded_letter_num)
+        decoded_word += decoded_letter
+    else:
+        encoded_letter_num = (ord(letter)-97) % 26 + 97
+        encoded_letter_num -= num_to_shift
+        decoded_letter = chr(encoded_letter_num)
+        decoded_word += decoded_letter
 print("\nDecoded word: " + decoded_word)
 
 print("\nThank you for using the Caesar Cipher Encoder!")
